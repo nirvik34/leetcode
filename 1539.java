04ea12c38b18da6557befee98e9c62ce3d08d4c4
nirvik34 <jarhnirvik@gmail.com>
2025-06-08 13:10:31 +0530

    Create 1539.java

---
+++ 1539.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/kth-missing-positive-number/
+
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int missing = 0;
+        int current = 1;
+        int index = 0;
+        while(missing<k){
+            if(index< arr.length && arr[index] == current){
+                index++;
+            }else{
+                missing++;
+                if(missing == k){
+                    return current;
+                }
+                
+            }
+            current++;
+            
+        }
+        return -1;
+        
+    }
+}
